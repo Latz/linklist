6be7deac69d117db4f8eb3ea4db5a9eafdf28767
Latz <latz@elektroelch.de>
2026-07-26 23:11:23 +0200

    Modernize linklist.js syntax and add JSDoc
    Replace new Array() with a literal, use strict equality and template
    literals, and document the two event-handler functions with @listens
    per JSDoc conventions. Behavior verified unchanged by the existing
    vitest suite.

--- linklist.js
+++ linklist.js
@@ -1,47 +1,49 @@
 const $j = jQuery.noConflict();
 
+/**
+ * Submits the bulk-edit LinkList state for the selected posts via AJAX.
+ * @listens click#bulk_edit
+ */
+$j( '#bulk_edit' ).click( function () {
+	const $bulk_row = $j( '#bulk-edit' );
+	const post_ids = [];
 
-$j( '#bulk_edit' ).click( function() {
-    const $bulk_row = $j( '#bulk-edit' );
-    const $post_ids = new Array();
+	// get the IDs of the selected posts
+	$bulk_row.find( '#bulk-titles' ).children().each( function () {
+		post_ids.push( $j( this ).attr( 'id' ).replace( /^(ttle)/i, '' ) );
+	} );
 
-    // get the IDS of the selected posts
-    $bulk_row.find( '#bulk-titles' ).children().each( function() {
-        $post_ids.push( $j( this ).attr( 'id' ).replace( /^(ttle)/i, '' ) );
-    });
+	// get the desired state of linklist
+	const linklist_display = $j( '#linklist-bulk-selectbox' ).val();
 
-    // get the desired state of linklist
-    const linklist_display = $j('#linklist-bulk-selectbox').val();
-
-    // save the data
-    $j.ajax({
-        url: ajaxurl,
-        type: 'POST',
-        async: false,
-        cache: false,
-        data: {
-            action: 'linklist_save_bulk_edit',
-            nonce: linklistBulkEdit.nonce,
-            post_ids: $post_ids,
-            linklist_state: linklist_display
-        }
-    });
-
-});
+	// save the data
+	$j.ajax( {
+		url: ajaxurl,
+		type: 'POST',
+		async: false,
+		cache: false,
+		data: {
+			action: 'linklist_save_bulk_edit',
+			nonce: linklistBulkEdit.nonce,
+			post_ids,
+			linklist_state: linklist_display,
+		},
+	} );
+} );
 
 const $wp_inline_edit = inlineEditPost.edit;
-inlineEditPost.edit = function( id ) {
 
-    $wp_inline_edit.apply( this, arguments );
-    let post_id = 0;
-    if ( typeof( id ) == 'object' )
-        post_id = Number.parseInt( this.getId( id ) );
+/**
+ * Wraps WordPress's quick-edit handler to sync the LinkList selectbox
+ * with the edited post's current LinkList state.
+ * @listens inlineEditPost#edit
+ */
+inlineEditPost.edit = function ( id ) {
+	$wp_inline_edit.apply( this, arguments );
 
-    const linklist_display = '#linklist-' + post_id;
-    const state = $j(linklist_display).html();
-    if (state)
-        $j("#linklist-selectbox").val('yes');
-    else
-        $j("#linklist-selectbox").val('no');
+	const post_id = typeof id === 'object' ? Number.parseInt( this.getId( id ), 10 ) : 0;
+	const linklist_display = `#linklist-${ post_id }`;
+	const state = $j( linklist_display ).html();
 
+	$j( '#linklist-selectbox' ).val( state ? 'yes' : 'no' );
 };
